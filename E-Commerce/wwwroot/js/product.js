298516c224eaa71c669043192274094a684a7952
AhmedEisa <AhmedEisa@DESKTOP-O9V8KLJ>
2023-10-12 00:06:53 +0300

    m

--- E-Commerce/wwwroot/js/product.js
+++ E-Commerce/wwwroot/js/product.js
@@ -2,13 +2,13 @@
 
 function addToCard(id) {
     console.log(id);
-
+    var cartCounter = document.getElementById("Cart-counter");
         $.ajax(
     {
         url: "/Product/addCard/" + id,
     success: function (result) {
         console.log(result);
-        console.log("His");
+        console.log(cartCounter);
 
                     }
                 });
