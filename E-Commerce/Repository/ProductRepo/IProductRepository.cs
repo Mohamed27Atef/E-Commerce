893e28980076bf04d6fc02dd4089da1c0aaa4e0e
Mohamed27Atef <mohamed27atef@gmail.com>
2023-10-14 13:19:08 +0300

    -check the error  of the js of eisa task , -check the problem of illegal characters in path which i didn't recogonize, -i can't test the project after some edit's of today but it was working yesterday.

--- E-Commerce/Repository/ProductRepo/IProductRepository.cs
+++ E-Commerce/Repository/ProductRepo/IProductRepository.cs
@@ -7,6 +7,9 @@ namespace E_Commerce.Repository.ProductRepo
     {
         Product getByName(string name);
         Product getByBrand(string brand);
-      
+        void AddToFavorites(int productId, int userId);
+        List<Product> GetFavoriteProductsForUser(string userId);
+
+
     }
 }
